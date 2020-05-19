aedbe23033c1f133586c0241b3585b57f5fbbea4
babyfarkmcgizax <babyfarkmcgizax@gmail.com>
2020-05-19 16:37:16 +0100

    fixed database logon - currently tweaking firewall
    VS2019 updated and lost new code :/

--- Models/CustomerDataAccessLayer.cs
+++ Models/CustomerDataAccessLayer.cs
@@ -10,7 +10,7 @@ namespace OltcoApp.Models
 {
     public class CustomerDataAccessLayer
     {
-        string connectionString = "Server=tcp:oltcoserver.database.windows.net,1433;Initial Catalog=oltcoDatabase;Persist Security Info=False;User ID=emmag;Password=Flooring1!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        string connectionString = "Server=tcp:oltcotest1.database.windows.net,1433;Initial Catalog=OltcoTest1;Persist Security Info=False;User ID=emmagreening;Password=Muldermoo1!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
         // adding Installation Details
         //To View all customer and installation details    
